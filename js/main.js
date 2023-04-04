a9426c2bb70abf3611b504ced3a724ccf25a8506
Alice Hinton-Jones <33206232+Lasirena@users.noreply.github.com>
2023-04-04 02:44:24 +0100

    JavaScript for Filters
    - Added functioning projects filter with JavaScript;

--- js/main.js
+++ js/main.js
@@ -1,3 +1,58 @@
+// Unhide all elements with .project class
+function unhideAllProjects() {
+    var allProjects = document.getElementsByClassName("project");
+    for (let project of allProjects) {
+        if (project.classList.contains("hidden")) {
+            project.classList.remove("hidden");
+        }
+    }
+}
+
+// Remove active marker from all filter buttons
+function deactivateFilterButtons() {
+    var allFilterButtons = document.getElementsByClassName("projects-filter");
+    for (let button of allFilterButtons) {
+        if (button.classList.contains("projects-filter-active")) {
+            button.classList.remove("projects-filter-active");
+        }
+    }
+}
+
+// Filter projects display to only one selected category
+function filterProjects(filterCategoryLabel, filterButtonId) {
+    unhideAllProjects();
+    deactivateFilterButtons();
+
+    // Activate the corresponding button
+    var buttonElement = document.getElementById(filterButtonId);
+    if (!buttonElement.classList.contains("projects-filter-active")) {
+        buttonElement.classList.add("projects-filter-active");
+    }
+
+    // Hide irrelevant projects
+    var allProjects = document.getElementsByClassName("project");
+    for (let project of allProjects) {
+        if (!project.classList.contains(filterCategoryLabel)) {
+            project.classList.add("hidden");
+        }
+    }
+
+    AOS.refresh();
+}
+
+// Unfilter projects display
+function removeProjectFilters(buttonLabel) {
+    unhideAllProjects();
+    deactivateFilterButtons();
+
+    // Mark the unfilter button as active
+    var button = document.getElementById(buttonLabel);
+    button.classList.add("projects-filter-active");
+
+    AOS.refresh();
+}
+
+// Show/Hide extra project details
 function toggleContentVisibility(elementId, buttonId) {
     var element = document.getElementById(elementId);
     var button = document.getElementById(buttonId);
@@ -8,4 +63,6 @@ function toggleContentVisibility(elementId, buttonId) {
         element.className = "hidden";
         button.innerText = "Show Details";
     }
+
+    AOS.refresh();
 }
