52ad0606f33425326686244945144456383d438a
Alice Hinton-Jones <33206232+Lasirena@users.noreply.github.com>
2023-10-16 00:17:59 +0100

    Navbar site name fix + styling for home page components

--- src/react-components/Navbar.tsx
+++ src/react-components/Navbar.tsx
@@ -4,10 +4,10 @@ function Navbar() {
   return (
     <header className="header" id="header">
       <div className="header-container">
-        <a href="" className="site-title">
+        <Link to={'/'} className="site-title">
           {' '}
           Alice Hinton-Jones{' '}
-        </a>
+        </Link>
         <div className="nav-mobile-button">
           <div className="nav-mobile-bar-1"></div>
           <div className="nav-mobile-bar-2"></div>
